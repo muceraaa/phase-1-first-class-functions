77f91b00a0f24d38d4549d6e5dc561cfee1fb8bb
valma <muceravn@gmail.com>
2022-06-12 19:34:33 +0300

    commit tree 5980d3c889008bed9b47a600c55a4c4d3aa28dfe

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFn() {
+      console.log("This is a named function.");
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return () => console.log("This is an anonymous function.");
+  }
